d59a32d45146e7f7c734b3ab31add425efdfc01c
c6burns <chris@fursman.com>
2019-06-16 04:16:01 -0600

    added endpoint getter and size funcs

--- include/socklynx/endpoint.h
+++ include/socklynx/endpoint.h
@@ -12,20 +12,48 @@
 #endif
 
 union socklynx_endpoint_u {
-	struct sockaddr_in addr;
+	struct sockaddr_in addr4;
 	struct sockaddr_in6 addr6;
 };
 
 typedef union socklynx_endpoint_u socklynx_endpoint_t;
 
+SL_INLINE int socklynx_endpoint_size(socklynx_endpoint_t *endpoint)
+{
+	SL_ASSERT(endpoint);
+	switch (endpoint->addr4.sin_family) {
+	case AF_INET:
+		return sizeof(struct sockaddr_in);
+	case AF_INET6:
+		return sizeof(struct sockaddr_in6);
+	}
+	return SL_ERR;
+}
+
 SL_INLINE bool socklynx_endpoint_is4(socklynx_endpoint_t *endpoint)
 {
-	return (endpoint->addr.sin_family == AF_INET);
+	SL_ASSERT(endpoint);
+	return (endpoint->addr4.sin_family == AF_INET);
+}
+
+SL_INLINE struct sockaddr_in *socklynx_endpoint_get4(socklynx_endpoint_t *endpoint)
+{
+	SL_ASSERT(endpoint);
+	SL_ASSERT(socklynx_endpoint_is4(endpoint));
+	return &endpoint->addr4;
 }
 
 SL_INLINE bool socklynx_endpoint_is6(socklynx_endpoint_t *endpoint)
 {
-	return (endpoint->addr.sin_family == AF_INET6);
+	SL_ASSERT(endpoint);
+	return (endpoint->addr4.sin_family == AF_INET6);
+}
+
+SL_INLINE struct sockaddr_in6 *socklynx_endpoint_get6(socklynx_endpoint_t *endpoint)
+{
+	SL_ASSERT(endpoint);
+	SL_ASSERT(socklynx_endpoint_is6(endpoint));
+	return &endpoint->addr6;
 }
 
 #endif
