6729e837e328e193501c67d00e83ef830b7364e6
c6burns <chris@fursman.com>
2019-06-17 21:41:59 -0600

    fixed copypasta and updated defs

--- include/socklynx/error.h
+++ include/socklynx/error.h
@@ -26,18 +26,18 @@
 #define SL_OK 0
 #define SL_ERR -1
 
-#ifdef SL_NO_ASSERT
+#ifdef SL_C_MISSING_ASSERT
 #	define SL_ASSERT(expr) ((void)0)
 #else
 #	include <assert.h>
 #	define SL_ASSERT(expr) assert(expr)
 #endif
 
-#define SL_GUARD(expr) if ((expr)) return TN_ERROR
-#define SL_GUARD_NULL(expr) if (!(expr)) return TN_ERROR
+#define SL_GUARD(expr) if ((expr)) return SL_ERR
+#define SL_GUARD_NULL(expr) if (!(expr)) return SL_ERR
 #define SL_GUARD_GOTO(lbl, expr) { if ((expr)) goto lbl; }
-#define SL_GUARD_CLEANUP(expr) TN_GUARD_GOTO(cleanup, expr)
-#define SL_GUARD_NULL_CLEANUP(expr) TN_GUARD_GOTO(cleanup, !(expr))
+#define SL_GUARD_CLEANUP(expr) SL_GUARD_GOTO(cleanup, expr)
+#define SL_GUARD_NULL_CLEANUP(expr) SL_GUARD_GOTO(cleanup, !(expr))
 
 
 
