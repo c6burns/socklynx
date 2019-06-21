e1a8fe8a9f1365d5f6a10220fc43dfd71e40ea3c
c6burns <chris@fursman.com>
2019-06-20 19:04:52 -0600

    added sockaddr getter and fixed size bug

--- src/socklynx/endpoint.c
+++ src/socklynx/endpoint.c
@@ -13,6 +13,12 @@ SL_INLINE_IMPL uint16_t sl_endpoint_af_set(sl_endpoint_t *endpoint, int32_t af)
 	return endpoint->addr4.sin_family;
 }
 
+SL_INLINE_DECL struct sockaddr *sl_endpoint_addr_get(sl_endpoint_t *endpoint)
+{
+	SL_ASSERT(endpoint);
+	return (const struct sockaddr *)endpoint;
+}
+
 SL_INLINE_IMPL bool sl_endpoint_is_ipv4(sl_endpoint_t *endpoint)
 {
 	SL_ASSERT(endpoint);
@@ -29,6 +35,6 @@ SL_INLINE_IMPL int sl_endpoint_size(sl_endpoint_t *endpoint)
 {
 	SL_ASSERT(endpoint);
 	if (sl_endpoint_is_ipv4(endpoint)) return sizeof(sockaddr4);
-	if (sl_endpoint_is_ipv4(endpoint)) return sizeof(sockaddr6);
+	if (sl_endpoint_is_ipv6(endpoint)) return sizeof(sockaddr6);
 	return SL_ERR;
 }
