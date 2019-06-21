2d9a57cac81fdbfa4c3bf12983360b9a6e9b169b
c6burns <chris@fursman.com>
2019-06-20 21:07:54 -0600

    added license spam

--- src/socklynx/endpoint.c
+++ src/socklynx/endpoint.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2019 Chris Burns <chris@kitty.city>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "socklynx/endpoint.h"
 
 
@@ -16,7 +38,7 @@ SL_INLINE_IMPL uint16_t sl_endpoint_af_set(sl_endpoint_t *endpoint, int32_t af)
 SL_INLINE_DECL struct sockaddr *sl_endpoint_addr_get(sl_endpoint_t *endpoint)
 {
 	SL_ASSERT(endpoint);
-	return (const struct sockaddr *)endpoint;
+	return (struct sockaddr *)endpoint;
 }
 
 SL_INLINE_IMPL bool sl_endpoint_is_ipv4(sl_endpoint_t *endpoint)
