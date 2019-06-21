ae0c886760dd439a58657d24eb7b59db904c2979
c6burns <chris@fursman.com>
2019-06-20 19:48:28 -0600

    added base netcore proj

---
+++ csharp/SockLynx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SL
+{
+	public static class SockLynx
+	{
+		public static int Setup()
+		{
+			return 0;
+		}
+
+		public static int Cleanup()
+		{
+			return 0;
+		}
+	}
+}
